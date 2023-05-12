b6a62120acf532854478083ae4d60077a834c678
pretam jayaswal <pretamjayaswal@gmail.com>
2023-05-12 23:40:34 +0530

    edited

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 config = {
-    "google_api_key":"AIzaSyBWWYo6ki7lifjVqpTThHxjsYxdoPYAm7E",
-    'youtube_playlist_id':'PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p',
+    "google_api_key":"",
+    'youtube_playlist_id':'PLYSbyG8n2PvmHXUkg6_jUacQAXf-NRkmw'
 }
